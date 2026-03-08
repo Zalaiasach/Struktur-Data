e3d55311cc0c6717e7a7991eac6b17bf203ad425
Afriezal Laiasach <afrizallaiasach16@gmail.com>
2026-03-08 12:52:55 +0700

    Hasil tugad 4 Pillar OOP

--- java/Main/src/App.java
+++ java/Main/src/App.java
@@ -1,5 +1,81 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Handphone HPBaru1=new HPFlagship("Samsung", "S26 Ultra");
+        HPBaru1.CekHarga(20000000);
+        Handphone HPBaru2=new HPMidRange("Oppo", "Reno 14");
+        HPBaru2.CekHarga(5000000);
+        Handphone HPLama=new HPEntryLevel("Xiaomi", "Redmi 14c");
+        HPLama.CekHarga(1500000);
+
+        HPBaru1.displayInfo();
+        System.out.println("Harganya= "+HPBaru1.DapatHarga());
+        HPBaru1.CekHP();
+        System.out.println("");
+        HPBaru2.displayInfo();
+        System.out.println("Harganya= "+HPBaru2.DapatHarga());
+        HPBaru2.CekHP();
+        System.out.println("");
+        HPLama.displayInfo();
+        System.out.println("Harganya= "+HPLama.DapatHarga());
+        HPLama.CekHP();
+    }
+}
+
+abstract class Handphone{
+    protected  String Brand;
+    protected  String Type;
+    private int Harga;
+    
+    public Handphone(String Brand, String Type){
+        this.Brand=Brand;
+        this.Type=Type;
+    }
+
+    public void CekHarga(int Harga){
+        this.Harga=Harga;
+    }
+
+    public int DapatHarga(){
+        return Harga;
+    }
+
+    public void displayInfo(){
+        System.out.println("Brand: "+Brand);
+        System.out.println("Type: "+Type);
+    }
+
+    abstract void CekHP();
+}
+
+class HPFlagship extends Handphone{
+    public HPFlagship(String Brand, String Type){
+        super(Brand,Type);
+    }
+
+    @Override
+    void CekHP(){
+        System.out.println("Bisa buat main Game");
+    }
+}
+
+class HPEntryLevel extends Handphone{
+    public HPEntryLevel (String Brand, String Type){
+        super(Brand,Type);
+    }
+
+    @Override
+    void CekHP(){
+        System.out.println("Kalau butuh saja");
+    }
+}
+
+class HPMidRange extends Handphone{
+    public HPMidRange (String Brand,String Type){
+        super (Brand,Type);
+    }
+
+    @Override
+    void CekHP(){
+        System.out.println("Kalau ada uang lebih");
     }
 }
